2e47878485d595ca70729bfa18a6cad35355a82a
Monika Rydstrom <87671051+monikat45@users.noreply.github.com>
2023-12-09 01:58:14 +0530

    Add files via upload

---
+++ Learning-Management-System/learn-management-backend/src/main/java/com/learn/management/LearnManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.airbus.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnManagementSpringApplication.class, args);
+	}
+
+}
