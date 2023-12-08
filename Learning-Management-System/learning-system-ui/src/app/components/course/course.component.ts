e3c47225e1dc91abca7aa82310e8ed51f84922bb
Monika Rydstrom <87671051+monikat45@users.noreply.github.com>
2023-12-09 02:20:10 +0530

    Update course.component.ts

--- Learning-Management-System/learning-system-ui/src/app/components/course/course.component.ts
+++ Learning-Management-System/learning-system-ui/src/app/components/course/course.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+// admin.component.ts
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CourseService } from 'path-to-course-service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class AdminComponent {
 
-  title = 'app';
-  constructor() { }
+    courseForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+    constructor(private fb: FormBuilder, private courseService: CourseService) {
+        this.courseForm = this.fb.group({
+            name: ['', Validators.required],
+            startDate: ['', Validators.required],
+            endDate: ['', Validators.required],
+        });
+    }
 
+    onCreateCourse() {
+        if (this.courseForm.valid) {
+            const courseData = this.courseForm.value;
+            this.courseService.createCourse(courseData).subscribe(
+                response => {
+                    console.log('Course creation successful');
+                    // Handle success, e.g., display a success message
+                },
+                error => {
+                    console.error('Course creation failed:', error);
+                    // Handle error, e.g., display an error message
+                }
+            );
+        }
+    }
 }
